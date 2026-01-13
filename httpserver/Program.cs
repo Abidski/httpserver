3a260549fd9f457994e3e6b795735034879330e0
Abid <abijanahmed@gmail.com>
2026-01-13 16:08:45 -0500

    Create Server classe

--- httpserver/Program.cs
+++ httpserver/Program.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace httpserver;
@@ -7,23 +8,11 @@ namespace httpserver;
 public class Program
 {
      static async Task<int> Main(string[] args)
-    {
-        TcpListener server = new TcpListener(IPAddress.Any, 8080);
-        try
-        {
-            server.Start();
-            using var handler = await server.AcceptTcpClientAsync();
-            var stream = handler.GetStream();
-            var response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello World!";
-            var bytes = Encoding.UTF8.GetBytes(response);
-            await stream.WriteAsync(bytes);
+     {
 
-        }
-        finally
-        {
-            server.Stop();
-        }
-
+         Server server = new Server();
+         await server.Start();
+         
         return 1;
 
     }
--- httpserver/Server.cs
+++ httpserver/Server.cs
@@ -1,8 +1,32 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 class Server
 {
+    private TcpListener _listener;
+    private bool isRunning;
     public Server()
     {
-        
+         _listener = new TcpListener(IPAddress.Any, 8080);
+         isRunning = true;
+    }
+
+    public async Task Start()
+    {
+        _listener.Start();
+        while (isRunning)
+        {
+            var handler = await _listener.AcceptTcpClientAsync();
+            Task handleClient = Task.Run(() => HandleClient(handler));
+        }
+    }
+
+    private async Task HandleClient(TcpClient handler)
+    {
+        var stream = handler.GetStream();
+        var response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello World!";
+        var bytes = Encoding.UTF8.GetBytes(response);
+        await stream.WriteAsync(bytes);
     }
 }
