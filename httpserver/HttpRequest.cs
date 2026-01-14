73e6ffa8e653c9302fc5f12e824c4235767bfc1f
Abid <abijanahmed@gmail.com>
2026-01-14 13:54:41 -0500

    Parse req method

---
+++ httpserver/HttpRequest.cs
@@ -0,0 +1,13 @@
+namespace httpserver;
+
+public class HttpRequest
+{
+    enum Method
+    {
+        Get,
+    }
+
+    public HttpRequest(string method, string uri, string version)
+    {
+    }
+}
--- httpserver/Server.cs
+++ httpserver/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using httpserver;
 
 class Server
 {
@@ -24,9 +25,81 @@ class Server
 
     private async Task HandleClient(TcpClient handler)
     {
-        var stream = handler.GetStream();
-        var response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello World!";
-        var bytes = Encoding.UTF8.GetBytes(response);
-        await stream.WriteAsync(bytes);
+        try
+        {
+
+            using (handler)
+            using (var stream = handler.GetStream())
+            using (var reader = new StreamReader(stream))
+            using (var writer = new StreamWriter(stream))
+            {
+                await ParseRequest(reader);
+                var message = "HTTP/1.1 200 OK\nContent-Type: text/plain; charset=utf-8\nContent-Length: 12\n\nHello World!";
+                var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(dateTimeBytes);
+         
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Error in handling client");
+        }
     }
+
+    private async Task ParseRequest(StreamReader reader)
+    {
+        try
+        {
+            var reqLine = await reader.ReadLineAsync();
+            CheckMethod(reqLine);
+            
+            while (!string.IsNullOrEmpty(reqLine))
+            {
+                reqLine = await reader.ReadLineAsync();
+                CheckHeader(reqLine);
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Error in Parse Request");
+            Console.WriteLine(e.Message);
+        }
+
+    }
+
+    private void CheckMethod(string line)
+    {
+        string [] words = line.Split(' ');
+        switch (words[0])
+        {
+           case "GET":
+               Console.WriteLine("Get method");
+               HandleGet(words);
+               break;
+           case "POST":
+               break;
+            case "PUT":
+               break;
+           case "PATCH":
+               break;
+           case "DELETE":
+               break;
+        }
+
+    }
+
+    private void CheckHeader(string line)
+    {
+        
+    }
+
+    private void HandleGet(string[] reqLine)
+    {
+        var method = reqLine[0];
+        var uri = reqLine[1];
+        var version = reqLine[2];
+        var HttpReq = new HttpRequest(method, uri, version);
+
+    }
+        
 }
